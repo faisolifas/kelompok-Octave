423fc1b1b3ee9851a06528ebd9ae9224247c0d7a
faisolifas <115636927+faisolifas@users.noreply.github.com>
2024-10-10 22:03:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementById("btn-tombol").addEventListener("click", function() {
+    
+    window.location.href = 'https://octavejaya.wordpress.com/?_gl=1*1xzzbvo*_gcl_au*MTUzOTE3MDUxOS4xNzI4MzcxOTY0';
+});
+
